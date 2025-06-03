92c1943459ed1b28e43de3019f508a3bf0cb146a
D Shivashankar <shivably04sdst@gmail.com>
2025-06-03 15:04:09 +0530

    url added for signout button

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -8,7 +8,7 @@
                 <h1 class="landing-page-heading mb-5">Hi!<br />Welcome to the <strong>Heritage Center!</strong></h1>
                 <div class="d-flex gap-3 justify-content-center align-items-center">
                     <a class="button mybutton" href="<?= BASE_URL?>data/test/1">Sign In</a>
-                    <a class="button mybutton active" href="#">Sign Out</a>
+                    <a class="button mybutton active" href="<?= BASE_URL?>data/profiles">Sign Out</a>
                 </div>
                 <p class="text-center"><img class="h-image" src="<?=PUBLIC_URL?>/images/home-footer.png" alt="" /></p>
             </div>
