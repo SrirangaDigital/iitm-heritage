9b034e9751656647b1ded4dd3491f36842cf743d
D Shivashankar <shivably04sdst@gmail.com>
2025-06-04 12:41:34 +0530

    added ui fixes and also added link to x button at the top right corner

--- application/views/forms/profiles.php
+++ application/views/forms/profiles.php
@@ -2,7 +2,7 @@
 <div class="container h-100">
     <div class="row  h-100 justify-content-center align-items-center">
         <div class="col-md-1 text-center">
-            <span><i class="bi bi-arrow-left-circle h3"></i></span>
+            <span><a href="<?=BASE_URL?>"><i class="bi bi-arrow-left-circle h3"></i></span></a>
         </div>    
         <div class="col-md-6 text-center">
             <p class="text-end my-5"><a href="<?=BASE_URL?>data/sign_in/99"><i class="bi bi-x h2"></i></a></p>
@@ -13,8 +13,8 @@
                 <div class="card">
                   <div class="card-body">
                     <h5 class="card-title"><?=$row['visitor_name']?></h5>
-                    <h6 class="card-subtitle"><?=(isset($row['visitor_count']))? $row['visitor_count']: '' ?></h6>
-                      <a href="<?=BASE_URL?>data/signout/<?=$row['id']?>" class="btn card-select-link">Select</a>
+                    <h6 class="card-subtitle"><?=(isset($row['visitor_count']))? $row['visitor_count']: '&nbsp;' ?></h6>
+                      <a href="<?=BASE_URL?>data/sign_out/<?=$row['id']?>" class="btn card-select-link">Select</a>
                   </div>
                 </div>
               <?php } ?>  
