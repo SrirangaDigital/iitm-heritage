7b1c7c0dd507bb988bd7e5fcc328470e98c77711
D Shivashankar <shivably04sdst@gmail.com>
2025-06-05 16:37:42 +0530

    tagging corrected

--- application/views/forms/common3.php
+++ application/views/forms/common3.php
@@ -3,7 +3,7 @@
     <div class="row  h-100 justify-content-center align-items-center">
         <div class="col-md-1 text-center">
             <!-- <span><i class="bi bi-arrow-left-circle h3"></i></span> -->
-            <span><a href="<?=BASE_URL?>data/sign_in/<?=$viewHelper->getHrefNumber($_SESSION['formdata']['visitor_type'])?>"><i class="bi bi-arrow-left-circle h3"></i></span></a>
+            <span><a href="<?=BASE_URL?>data/sign_in/<?=$viewHelper->getHrefNumber($_SESSION['formdata']['visitor_type'])?>"><i class="bi bi-arrow-left-circle h3"></i></a></span>
         </div>    
         <div class="col-md-6 text-center">
             <p class="text-end my-5"><a href="<?=BASE_URL?>data/sign_in/99"><i class="bi bi-x h2"></i></a></p>
