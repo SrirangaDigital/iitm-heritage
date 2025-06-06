9a58ae4b7988e052973a30239b7d53490dd3bd0f
D Shivashankar <shivably04sdst@gmail.com>
2025-06-06 12:05:32 +0530

    deleted unwanted files

--- application/views/error/blah.php
+++
@@ -1,3 +0,0 @@
-<h1>Sorry!</h1>
-<h2>&nbsp;</h2>
-<p>Something went wrong! Please try again after some time.</p>
--- application/views/error/noExternalResource.php
+++
@@ -1,2 +0,0 @@
-<i class="fa fa-warning"></i>
-<h2 class="noExternalResource">External resource not found</h2>
