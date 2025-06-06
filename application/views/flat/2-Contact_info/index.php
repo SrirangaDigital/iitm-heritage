f107134a0886f47b3efdd5743de996cfcc84c819
D Shivashankar <shivably04sdst@gmail.com>
2025-06-06 12:06:58 +0530

    removed unwanted files

--- application/views/flat/2-Contact_info/index.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row">
-        <!-- Column 1 -->
-        <div class="col-md-12 text-center">
-            <p>&nbsp;</p>    
-        </div>
-    </div>
-</div>
-
