be5ff8684d48211537dbc03f2ad0119e2de4665d
Shivashankar D <sriranga@Shivashankars-MacBook-Air.local>
2025-07-01 12:10:55 +0530

    commented unwanted code

--- application/views/forms/signout-4.php
+++ application/views/forms/signout-4.php
@@ -1,9 +1,9 @@
 <!-- Flow :  3 -->
 <div class="container h-100">
     <div class="row  h-100 justify-content-center align-items-center">
-        <!-- <div class="col-md-1 text-center"> -->
-            <!-- <span><a href="<?=BASE_URL?>data/sign_out/<?=$data[0]['id']?>/2"><i class="bi bi-arrow-left-circle h3"></i></a></span> -->
-        <!-- </div> -->
+<!--         <div class="col-md-1 text-center">
+            <span><a href="<?=BASE_URL?>data/sign_out/<?=$data[0]['id']?>/2"><i class="bi bi-arrow-left-circle h3"></i></a></span>
+        </div> -->
         <div class="col-md-6 text-center">
             <p class="text-end my-5"><a href="<?=BASE_URL?>data/sign_out/<?=$data[0]['id']?>/99"><i class="bi bi-x h2"></i></a></p>
             <form action="<?=BASE_URL?>data/sign_out/<?=$data[0]['id']?>/4" method="POST">
